633d6eda1b5f927804921f89547b10e2f9bd7e90
Paulo Davi <paulo.araujo@cear.ufpb.br>
2021-11-29 18:34:46 -0300

    finish challerger

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,21 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("game")
+      .where("game.title ilike :param", { param: `%${param}%` })
+      .getMany()
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT COUNT(*) FROM games');
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("game")
+      .leftJoinAndSelect("game.users", "users")
+      .where("game.id = :id", { id })
+      .getOne()
+      .then(game => game?.users || [])
   }
 }
